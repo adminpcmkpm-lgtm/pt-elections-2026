e9a0b48becb9f6534ee46851ab240506ad06f24f
adminpcmkpm-lgtm <adminpcmkpm-lgtm@users.noreply.github.com>
2026-08-21 09:30:18 +0530

    PT Elections 2026 - Voters List App

---
+++ sw.js
@@ -0,0 +1,11 @@
+const CV='ptel-v1';
+const ASSETS=['./manifest.json','./icons/icon-180.png','./icons/icon-192.png','./icons/icon-512.png'];
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CV).then(c=>c.addAll(ASSETS)))});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CV).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
+self.addEventListener('fetch',e=>{
+ if(e.request.method!=='GET')return;
+ e.respondWith(caches.match(e.request,{ignoreSearch:true}).then(hit=>hit||fetch(e.request).then(res=>{
+   try{const cp=res.clone();caches.open(CV).then(c=>c.put(e.request,cp));}catch(_){}
+   return res;
+ }).catch(()=>caches.match('./index.html'))));
+});
